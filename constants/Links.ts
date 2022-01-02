5c1190081d836b94f6d3741ba7b39e444db0f7bf
Daniel <daniel@miller.computer>
2022-01-02 17:31:47 -0600

    Added Links constants

---
+++ constants/Links.ts
@@ -0,0 +1,23 @@
+export const internalLinks = [
+  { label: "Home", url: "/" },
+  { label: "Improve Your System", url: "/improveyoursystem" },
+  { label: "Bits", url: "/bits" },
+  // { label: "Gems", url: "/gems" },
+  { label: "Tutorials", url: "/tutorials" },
+  { label: "In The News", url: "/inthenews" },
+  { label: "For New Users", url: "/fornewusers" },
+];
+export const externalLinks = [
+  {
+    label: "GitHub",
+    url: "https://github.com/BinaryBitLLC/binarybit",
+    filename: "github.svg",
+    hoverColor: "#5865F2",
+  },
+  {
+    label: "Discord",
+    url: "https://discord.gg/TYEs4qzstp",
+    filename: "discord.svg",
+    hoverColor: "#5865F2",
+  },
+];
