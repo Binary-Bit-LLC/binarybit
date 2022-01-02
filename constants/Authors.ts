3999d31e8b3b8b6e5adff88e3a6ed18f28d359cc
Daniel <daniel@miller.computer>
2022-01-02 17:31:09 -0600

    Added Author info

---
+++ constants/Authors.ts
@@ -0,0 +1,16 @@
+export const authors = {
+  danielmiller: {
+    id: "danielmiller",
+    label: "Daniel Miller",
+    website: "https://www.miller.computer",
+    bio: "",
+    page: true,
+  },
+  dn0r: {
+    id: "dn0r",
+    label: "dn0rius IV",
+    website: "https://www.microsoft.com",
+    bio: "",
+    page: true,
+  },
+};
