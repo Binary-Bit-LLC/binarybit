607969b9e8681d76fd29e2183a7b16880a90f140
Daniel <daniel@miller.computer>
2022-01-02 17:32:46 -0600

    Added main app page

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.sass";
+import type { AppProps } from "next/app";
+import Header from "../components/Header/Header";
+
+function BinaryBit({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default BinaryBit;
