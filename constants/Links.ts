bff569182290b5b963dd4e3f4f980dea99f115cc
Daniel <daniel@miller.computer>
2022-01-03 00:19:05 -0600

    Changed Discord to Matrix and updated hover colors

--- constants/Links.ts
+++ constants/Links.ts
@@ -12,12 +12,12 @@ export const externalLinks = [
     label: "GitHub",
     url: "https://github.com/BinaryBitLLC/binarybit",
     filename: "github.svg",
-    hoverColor: "#5865F2",
+    hoverColor: "#757575",
   },
   {
-    label: "Discord",
-    url: "https://discord.gg/TYEs4qzstp",
-    filename: "discord.svg",
-    hoverColor: "#5865F2",
+    label: "Matrix",
+    url: "https://matrix.to/#/#binary-bit:matrix.org",
+    filename: "matrix.svg",
+    hoverColor: "#757575",
   },
 ];
