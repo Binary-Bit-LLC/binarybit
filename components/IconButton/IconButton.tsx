4d77dd7ddd82ab240182ec4287eebee0bf26974c
syntheit <daniel@miller.computer>
2022-07-28 01:51:42 -0500

    Made use of tailwind

--- components/IconButton/IconButton.tsx
+++ components/IconButton/IconButton.tsx
@@ -3,16 +3,12 @@ import css from "styled-jsx/css";
 
 // I tried to get the icon color to change on hover, but for some reason,
 // it refused to change the fill.  There doesn't seem to be any fill
-// on the SVGs themselves, tried in devtools, checked to make sure the 
-// props are not null/undefined, etc.  
+// on the SVGs themselves, tried in devtools, checked to make sure the
+// props are not null/undefined, etc.
 // Please submit a PR if you know how to fix this
 
 const getButtonStyles = (hoverColor) => {
   return css.resolve`
-    img {
-      cursor: pointer;
-      margin-left: 24px;
-    }
     img:hover {
       fill: ${hoverColor};
     }
@@ -25,14 +21,13 @@ export default ({ metadata }) => {
     <div>
       {/* Why can't the link be displayed on hover and on right click? */}
       <a href={metadata.url}>
+        {/* {metadata.filename} */}
         <img
-          className={className}
+          className={["ml-6 w-8 cursor-pointer", className].join(" ")}
           src={`/images/${metadata.filename}`}
-          height={"30px"}
           alt={metadata.label} // why isn't this working...
         />
       </a>
-      {styles}
     </div>
   );
 };
